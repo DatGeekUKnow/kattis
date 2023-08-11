bba72ffb1375f1a5ad86efb7b6121f29eb971f9c
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2023-08-10 23:27:07 -0400

    Finished Units

--- Units.cpp
+++ Units.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <iomanip>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -15,30 +17,79 @@ int main ()
 {
     int num; // number of units
     string unit, unit2;
-    map<string, pair<string, double>> values;
+    vector<string> keys;
+    vector<vector<double>> values;
     while (cin >> num)
     {
+        if (num == 0) return 0;
         values = {};
+        keys = {};
+        values.resize(num);
+        for(auto& vect : values)
+        {
+            vect.resize(num);
+        }
+
+        // Initialize keys
         for (int i = 0; i < num; i++)
         {
             cin >> unit;
-            values[unit] = {};
+            keys.push_back(unit);
+            values[i][i] = 1;
         }
 
-        int val;
+        double val;
         char trash;
         for (int i = 1; i < num; i++)
         {
+            val = 0.0;
             cin >> unit;
-            cin >> trash;
+            cin >> trash; // throw away = sign
             cin >> val;
             cin >> unit2;
-            values[unit] = {unit2, val};
+            auto unitIndex = std::find(keys.begin(), keys.end(), unit);
+            auto unit2Index = std::find(keys.begin(), keys.end(), unit2);
+            values[unitIndex - keys.begin()][unit2Index - keys.begin()] = val;
+            values[unit2Index - keys.begin()][unitIndex - keys.begin()] = 1.0/val;
         }
 
-        for (auto& i : values)
+        for (int x = 0; x < keys.size(); ++x) 
         {
-            cout << i.first << endl;
+            for(int y = 0; y < keys.size(); ++y) 
+            {
+                for(int z = 0; z < keys.size(); ++z)
+                {
+                    if (values[z][y] == 0 && values[z][x] != 0)
+                    {
+                        values[z][y] = values[x][y] * values[z][x];
+                    }
+                }
+            }
+        }
+
+        cout << std::setprecision(11);        
+        for (auto& vec : values)
+        {
+            auto val = std::find_if(vec.begin(), vec.end(), [](double val) { return val < 1;});
+            if (val == vec.end()) 
+            { // this is our prize vector
+                // print in descending order
+                // match key index to keys vector
+                vector<double> newVals;
+                std::copy(vec.begin(), vec.end(), std::back_inserter(newVals));
+                std::sort(newVals.begin(), newVals.end());
+                
+                for(int i = 0; i < newVals.size(); ++i)
+                {
+                    auto itr = std::find(vec.begin(), vec.end(), newVals[i]);
+                    int index = itr - vec.begin();
+                    cout << vec[index] << keys[index];
+                    if (i < newVals.size() - 1) {
+                        cout << " = ";
+                    }
+                }
+                cout << endl;
+            }
         }
     }
     return 0;
